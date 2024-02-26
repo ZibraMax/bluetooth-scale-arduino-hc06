2a0ee9608889fba5de9f324265a180c7308372be
Arturo Rodriguez <davidarturo9911@hotmail.com>
2024-02-26 12:25:11 -0500

    It[s finished

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const button = document.getElementById("bton");
 const buttonTare = document.getElementById("tare");
 const buttonRestart = document.getElementById("buttonRestart");
 const buttonStop = document.getElementById("buttonStop");
+const buttonPause = document.getElementById("buttonPause");
+const buttonDownload = document.getElementById("buttonDownload");
 class LineBreakTransformer {
 	constructor() {
 		this.chunks = "";
@@ -17,6 +19,21 @@ class LineBreakTransformer {
 	flush(controller) {
 		controller.enqueue(this.chunks);
 	}
+}
+function download(filename, text) {
+	var element = document.createElement("a");
+	element.setAttribute(
+		"href",
+		"data:text/plain;charset=utf-8," + encodeURIComponent(text)
+	);
+	element.setAttribute("download", filename);
+
+	element.style.display = "none";
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
 }
 
 const dataMass = { x: [0], y: [0], mode: "lines", line: { shape: "spline" } };
@@ -45,81 +62,118 @@ const layout_vel = {
 		family: "Space Mono, monospace",
 	},
 };
-Plotly.newPlot("mass", [dataMass], layout_mass);
-Plotly.newPlot("flow", [dataFlow], layout_vel);
+
+const config = { responsive: true };
+var paused = false;
+Plotly.newPlot("mass", [dataMass], layout_mass, config);
+Plotly.newPlot("flow", [dataFlow], layout_vel, config);
 
 button.addEventListener("click", async () => {
+	let offsetTime = 0;
 	text.innerHTML = "Loading!";
-	const port = await navigator.serial.requestPort();
-	await port.open({ baudRate: 9600 });
-	const textDecoder = new TextDecoderStream();
-	const textEncoder = new TextEncoderStream();
-	const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
-	const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-	const reader = textDecoder.readable
-		.pipeThrough(new TransformStream(new LineBreakTransformer()))
-		.getReader();
-	const writer = textEncoder.writable.getWriter();
-	await writer.write("k");
-	await writer.write("t");
-	let connected = true;
+	try {
+		const port = await navigator.serial.requestPort();
+		await port.open({ baudRate: 9600 });
 
-	buttonRestart.addEventListener("click", async () => {
-		try {
-			writer.write("k");
-			writer.write("t");
-		} catch (error) {}
-		dataMass.x = [0];
-		dataMass.y = [0];
-		dataFlow.x = [0];
-		dataFlow.y = [0];
-		Plotly.redraw("mass");
-		Plotly.redraw("flow");
-	});
-	buttonTare.addEventListener("click", async () => {
-		writer.write("t");
-	});
-	buttonStop.addEventListener("click", async () => {
-		connected = false;
-		reader.cancel();
-		await readableStreamClosed.catch(() => {
-			/* Ignore the error */
+		const textDecoder = new TextDecoderStream();
+		const textEncoder = new TextEncoderStream();
+		const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+		const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+		const reader = textDecoder.readable
+			.pipeThrough(new TransformStream(new LineBreakTransformer()))
+			.getReader();
+		const writer = textEncoder.writable.getWriter();
+		await writer.write("t");
+		await writer.write("k");
+		await writer.write("s");
+		let connected = true;
+		buttonPause.addEventListener("click", async () => {
+			await writer.write("p");
+			if (paused) {
+				paused = false;
+				buttonPause.innerHTML = "Pause";
+			} else {
+				paused = true;
+				buttonPause.innerHTML = "Resume";
+			}
+		});
+		buttonDownload.addEventListener("click", async () => {
+			download(
+				`mass_${new Date().toJSON().slice(0, 19)}.json`.replaceAll(
+					":",
+					"-"
+				),
+				JSON.stringify(dataMass)
+			);
+			download(
+				`flow_${new Date().toJSON().slice(0, 19)}.json`.replaceAll(
+					":",
+					"-"
+				),
+				JSON.stringify(dataFlow)
+			);
 		});
 
-		writer.close();
-		await writableStreamClosed;
-
-		await port.close();
-	});
-	while (connected) {
-		const { value, done } = await reader.read();
-		if (done) {
-			reader.releaseLock();
-			break;
-		}
-		if (!value.includes("#")) {
-			let datos = value.split("//");
-			let tiempo = parseFloat(datos[0]) / 1000.0;
-			let masa = parseFloat(datos[1]);
-			let velocidad = parseFloat(datos[2]);
-			let n_datos = parseInt(datos[3]);
-			text.innerHTML = masa + " g";
-			textTime.innerHTML = tiempo + " s";
-
-			if (tiempo < dataFlow.x[dataFlow.x.length - 1]) {
-				dataMass.x = [0];
-				dataMass.y = [0];
-				dataFlow.x = [0];
-				dataFlow.y = [0];
-			}
-			dataMass.x.push(tiempo);
-			dataFlow.x.push(tiempo);
-			dataMass.y.push(masa);
-			dataFlow.y.push(velocidad);
+		buttonRestart.addEventListener("click", async () => {
+			try {
+				writer.write("k");
+				writer.write("t");
+			} catch (error) {}
+			dataMass.x = [0];
+			dataMass.y = [0];
+			dataFlow.x = [0];
+			dataFlow.y = [0];
 			Plotly.redraw("mass");
 			Plotly.redraw("flow");
-		} else {
-			text.innerHTML = value;
+		});
+		buttonTare.addEventListener("click", async () => {
+			writer.write("t");
+		});
+		buttonStop.addEventListener("click", async () => {
+			connected = false;
+			reader.cancel();
+			await readableStreamClosed.catch(() => {
+				/* Ignore the error */
+			});
+
+			writer.close();
+			await writableStreamClosed;
+
+			await port.close();
+		});
+		while (connected) {
+			const { value, done } = await reader.read();
+			if (done) {
+				reader.releaseLock();
+				break;
+			}
+			if (!value.includes("#")) {
+				let datos = value.split("//");
+				let tiempo = parseFloat(datos[0]) / 1000.0;
+				let masa = parseFloat(datos[1]);
+				let velocidad = parseFloat(datos[2]);
+				let n_datos = parseInt(datos[3]);
+				text.innerHTML = masa + " g";
+				textTime.innerHTML = tiempo + " s";
+
+				if (tiempo < dataFlow.x[dataFlow.x.length - 1]) {
+					dataMass.x = [0];
+					dataMass.y = [0];
+					dataFlow.x = [0];
+					dataFlow.y = [0];
+				}
+				tiempo -= offsetTime;
+				dataMass.x.push(tiempo);
+				dataFlow.x.push(tiempo);
+				dataMass.y.push(masa);
+				dataFlow.y.push(velocidad);
+				Plotly.redraw("mass");
+				Plotly.redraw("flow");
+			} else {
+				text.innerHTML = value;
+			}
 		}
+	} catch (error) {
+		text.innerHTML = "Device disconected! Try again!";
 	}
 });
--- test_nano.ino
+++ test_nano.ino
@@ -15,7 +15,8 @@ unsigned long starttime = millis();
 unsigned long start_timer = millis();
 float promedio = 0.0;
 int cantidad = 0;
-
+bool midiendo = true;
+unsigned long timeOffset = 0;
 void setup()
 {
   Serial.begin(9600);
@@ -27,6 +28,8 @@ void setup()
   scale.tare();
   Serial.println("Balanza lista!");
   hc06.println("#Balanza lista!");
+  starttime = millis();
+  start_timer = millis();
 }
 
 void tarar()
@@ -64,10 +67,28 @@ void manipulateInput(int input)
     Serial.println("Delay: " + String(((float)delaytime) / 1000));
     hc06.println("#Delay: " + String(((float)delaytime) / 1000));
   }
-  else if (input == (int) 'k')
+  else if (input == (int)'k')
   {
     start_timer = millis();
     Serial.println("Timer en 0!");
+  }
+  else if (input == (int)'p')
+  {
+    if (midiendo)
+    {
+      midiendo = false;
+      timeOffset = millis() - start_timer;
+    }
+    else
+    {
+      midiendo = true;
+
+      start_timer = millis() - timeOffset;
+    }
+  }
+  else if (input == (int)'s')
+  {
+    midiendo=true;
   }
 }
 
@@ -82,30 +103,32 @@ void loop()
     int btmsg = hc06.read();
     manipulateInput(btmsg);
   }
-  cantidad++;
-  float actual = scale.get_units();
-  promedio += (actual - promedio) / cantidad;
-
-  unsigned long endtime = millis();
-  unsigned long duration_timer = endtime - start_timer;
-  unsigned long duration = endtime - starttime;
-  if (duration >= delaytime)
+  if (midiendo)
   {
-    starttime = endtime;
+    cantidad++;
+    float actual = scale.get_units();
+    promedio += (actual - promedio) / cantidad;
 
-    float vel = (promedio - last) / ((float)duration) * 1000;
-    last = promedio;
-    if (abs(promedio) < 0.1)
+    unsigned long endtime = millis();
+    unsigned long duration_timer = endtime - start_timer;
+    unsigned long duration = endtime - starttime;
+    if (duration >= delaytime)
     {
-      promedio = 0;
-      vel = 0;
-    }
+      starttime = endtime;
 
-    String msg1 = String(promedio, 1) + " g. " + String(vel, 1) + " g/s. " + String(cantidad) + " mediciones.";
-    String msg2 = String(duration_timer)+"//"+ String(promedio, 1) + "//" + String(vel, 1) + "//" + String(cantidad) + "//";
-    Serial.println(msg1);
-    hc06.println(msg2);
-    reset();
+      float vel = (promedio - last) / ((float)duration) * 1000;
+      last = promedio;
+      if (abs(promedio) < 0.1)
+      {
+        promedio = 0;
+        vel = 0;
+      }
+
+      String msg1 = String(promedio, 1) + " g. " + String(vel, 1) + " g/s. " + String(cantidad) + " mediciones. Reloj " + String(duration_timer);
+      String msg2 = String(duration_timer) + "//" + String(promedio, 1) + "//" + String(vel, 1) + "//" + String(cantidad) + "//";
+      Serial.println(msg1);
+      hc06.println(msg2);
+      reset();
+    }
   }
-  
 }
